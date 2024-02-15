de2a2e64e3a2c2ef99c1567dcd726f70eda18eb6
Tshepiso Motshele <tshepisomotshele9@gmail.com>
2024-02-15 19:17:00 +0200

    added content on the achievements page

--- finaltest/src/App.js
+++ finaltest/src/App.js
@@ -3,10 +3,10 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import "./App.css"
 
 import  Home  from './Pages/HomePage/Home.jsx';
-import About from './Pages/AboutPage/About.jsx'
-import Portfolio from './Pages/PortfoliosPage/Portfolio.jsx';
-import { Achievement } from './Pages/Achievements/Achievement';
-import { Contact } from './Pages/ContactPage/Contact';
+//import About from './Pages/AboutPage/About.jsx'
+//import Portfolio from './Pages/PortfoliosPage/Portfolio.jsx';
+//import Achievement from './Pages/Achievements/Achievement.jsx';
+//import { Contact } from './Pages/ContactPage/Contact';
 
 
 
@@ -17,12 +17,9 @@ function App() {
       <div>
         
         <Routes>
-          <Route path="/" element={<Home />} />
+          
           <Route path="/home" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/Portfolio" element={<Portfolio />} />
-          <Route path="/Achievement" element={<Achievement />} />
-          <Route path="/Contact" element={<Contact />} />
+      
         </Routes>
       </div>
     </Router>
--- finaltest/src/Pages/Achievements/Achievement.jsx
+++ finaltest/src/Pages/Achievements/Achievement.jsx
@@ -1,76 +1,35 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import "./Features.css";
+import data from "./Features-Api";
+import Card from "./FeaturesCard";
 
-import "./Achievements.css";
-
-export const Achievement = () => {
+const Achievement = () => {
   return (
-    <div className="achievement">
-      <div className="home-wrapper">
-        <div className="home">
-          <div className="overlap-9">
-            <div className="nav-bar">
-              <div className="overlap-group-9">
-                <div className="rectangle-10" />
-                <NavLink to="/" className="text-wrapper-23" activeClassName="active">Home</NavLink>
-                <NavLink to="/about" className="text-wrapper-24" activeClassName="active">About</NavLink>
-                <NavLink to="/achievement" className="text-wrapper-20" activeClassName="active">Achievement</NavLink>
-                <NavLink to="/contact" className="text-wrapper-21" activeClassName="active">Contact</NavLink>
-                <NavLink to="/portfolio" className="text-wrapper-22" activeClassName="active">Portfolio</NavLink>
-                
-                <img
-                  className="home-svgrepo-com"
-                  alt="Home svgrepo com"
-                  src="https://cdn.animaapp.com/projects/65c1f19fe83795b5da57d4a2/releases/65cbd681bc2ac91615baa1b2/img/home-1-svgrepo-com.svg"
-                />
-                <img
-                  className="about-information"
-                  alt="About information"
-                  src="https://cdn.animaapp.com/projects/65c1f19fe83795b5da57d4a2/releases/65cbd681bc2ac91615baa1b2/img/about-information-info-help-svgrepo-com.svg"
-                />
-                <img
-                  className="portfolio-svgrepo"
-                  alt="Portfolio svgrepo"
-                  src="https://cdn.animaapp.com/projects/65c1f19fe83795b5da57d4a2/releases/65cbd681bc2ac91615baa1b2/img/portfolio-svgrepo-com.svg"
-                />
-                <img
-                  className="education-svgrepo"
-                  alt="Education svgrepo"
-                  src="https://cdn.animaapp.com/projects/65c1f19fe83795b5da57d4a2/releases/65cbd681bc2ac91615baa1b2/img/education-svgrepo-com.svg"
-                />
-                <img
-                  className="location-place-pin"
-                  alt="Location place pin"
-                  src="https://cdn.animaapp.com/projects/65c1f19fe83795b5da57d4a2/releases/65cbd681bc2ac91615baa1b2/img/location-place-pin-svgrepo-com.svg"
-                />
-                <img
-                  className="vector-2"
-                  alt="Vector"
-                  src="https://cdn.animaapp.com/projects/65c1f19fe83795b5da57d4a2/releases/65cbd40a1e494c73352c49f5/img/vector-1.svg"
-                />
-                <img
-                  className="vector-3"
-                  alt="Vector"
-                  src="https://cdn.animaapp.com/projects/65c1f19fe83795b5da57d4a2/releases/65c1f95a886b7f388e40c1d9/img/vector-4.svg"
-                />
-                <p className="text-wrapper-25">
-                  2024 | Designed and Developed By Tshepiso Motshele. | All rights reserved.
-                </p>
-              </div>
-            </div>
-            <img
-              className="element"
-              alt="Element"
-              src="https://cdn.animaapp.com/projects/65c1f19fe83795b5da57d4a2/releases/65c2618037c6cf2659498171/img/147166387-1@2x.png"
-            />
-            <div className="rectangle-11" />
-              <div className="node-JS-instance" />
-              <div className="java-instance" />
-            <div className="text-wrapper-26">My Wall Of Achievements</div>
+    <>
+      <section className='achievement top' id='achievement'>
+        <div className='container'>
+          <div className='heading'>
+            <h4>achievement</h4>
+            <h1>What I Do</h1>
+          </div>
+
+          <div className='content grid'>
+            {data.map((val, index) => {
+              return <Card key={index} image={val.image} title={val.title} desc={val.desc} />;
+            })}
+
+            {/*<div className='box btn_shadow'>
+              <img src='https://img.icons8.com/glyph-neue/64/000000/polyline.png' alt='' />
+              <h2>Personal Portfolio April</h2>
+              <p>It uses a dictionary of over 200 Latin words, combined with a handful of model sentence.</p>
+              <a href=''>
+                <i class='fas fa-arrow-right'></i>
+              </a>
+            </div>*/}
           </div>
         </div>
-      </div>
-    </div>
+      </section>
+    </>
   );
 };
 
