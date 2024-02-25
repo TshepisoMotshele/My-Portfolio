ab4015346b0008d6ff5f435fc2d3b06f03fb70a0
Tshepiso Motshele <tshepisomotshele9@gmail.com>
2024-02-25 12:25:45 +0200

    .

--- finaltest/src/components/contact/SocialData.jsx
+++ finaltest/src/components/contact/SocialData.jsx
@@ -1,5 +1,5 @@
 export const socialsData = {
-    Discord: "https://wa.me/8171280446",
-    github: "https://github.com/MehraDevesh2022",
-    linkedIn: "https://www.linkedin.com/in/iam-devesh/",
+    Discord: "#",
+    github: "https://github.com/TshepisoMotshele",
+    linkedIn: "https://www.linkedin.com/feed/",
   };
